3aa4e4fa6e2f8e64af7c1960269734b7824d3a4d
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2025-02-04 02:45:01 +0300

    Класс для взаимодействия с Mistral AI (Class for interacting with Mistral AI)

---
+++ ai_client.py
@@ -0,0 +1,41 @@
+from mistralai import Mistral
+
+
+class MistralClient:
+    """
+    Класс для взаимодействия с Mistral AI
+    """
+
+    def __init__(self, api_key, model, legend, assistant_name, gender, language):
+        """
+        Конструктор
+        :param api_key: api ключ
+        :param model: модель Mistral
+        :param legend: контекст для ai
+        :param assistant_name: имя ассистента
+        :param gender: пол ассистента
+        :param language: язык ассистента(по умолчанию русский так как модель распознания речи только для русского языка)
+        """
+        self.client = Mistral(api_key=api_key)
+        self.model = model
+        self.legend = legend
+        self.assistant_name = assistant_name
+        self.gender = gender
+        self.language = language
+
+    def request_to_ai(self, question):
+        """
+        Запрос к Mistral AI
+        :param question: текст запроса
+        :return: ответ от Mistral AI
+        """
+        legend = self.legend.format(self.assistant_name, "женского" if self.gender == "female" else "мужского",
+                                    "русском" if self.language == "ru" else self.language)
+        chat_response = self.client.chat.complete(
+            model=self.model,
+            messages=[
+                {'role': 'system', 'content': legend},
+                {'role': 'user', 'content': f'{question}'}
+            ]
+        )
+        return chat_response.choices[0].message.content
