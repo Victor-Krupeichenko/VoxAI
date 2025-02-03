3c6f4ea59e53ebad178aae794b55d8787695b131
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2025-02-04 00:52:03 +0300

    чтение файла с настройками проекта (reading a file settings)

---
+++ load_config.py
@@ -0,0 +1,46 @@
+class SettingsLoader:
+    def __init__(self, path):
+        self.path = path
+
+    @staticmethod
+    def read_file(file):
+        """
+        Построчно читает полученный файл
+        :param file: имя файла
+        :return: строку
+        """
+        for st in file:
+            yield st.strip()
+
+    @staticmethod
+    def is_float(value):
+        """
+        Проверяет являться ли строка числом с плавающей точкой
+        :param value: строка
+        :return: bool
+        """
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+
+    def open_file(self):
+        """
+        Открывает файл и построчно читает содержимое.
+        :return: Словарь с настройками приложения
+        """
+        conf_dict = dict()
+        try:
+            with open(self.path, "r", encoding="utf-8") as file:
+                for st in self.read_file(file):
+                    key, value = st.split("=", 1)
+                    key, value = key.strip(), value.strip()
+                    if value.isdigit():
+                        value = int(value)
+                    elif self.is_float(value):
+                        value = float(value)
+                    conf_dict.setdefault(key, value)
+        except FileNotFoundError:
+            print("Файл не найден")
+        return conf_dict
